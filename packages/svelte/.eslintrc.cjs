18c33e9b0981cf2e5535f971d69cffa391d34a3d
Prashan <prashans917@gmail.com>
2026-05-16 14:42:52 +0530

    style: fix prettier formatting

--- packages/svelte/.eslintrc.cjs
+++ packages/svelte/.eslintrc.cjs
@@ -1,12 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  extends: ["zerithdb-eslint-config"],
   env: {
     browser: true,
     es2022: true,
